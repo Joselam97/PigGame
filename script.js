fb55fecdcd869c3bb046fb6a10ffd50d8d7579d1
Jose Alvarez <jlam97@hotmail.es>
2024-11-08 21:24:19 -0600

    Implementacion de logica que el juego se detenga y esconda el dado una vez alguien llegue a 100pts

--- script.js
+++ script.js
@@ -16,28 +16,34 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+
 //Iniciando las variables
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+
 //Inicializa puntajes para activePlayer
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true; //variable para permitir seguir jugando o no
+
 
 //funcion para cambiar de player
-const switchPlayer = function(){
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
 
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-}
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 
 //Funcionalidad al dado
-btnRoll.addEventListener('click', function(){
+btnRoll.addEventListener('click', function () {
+  if (playing) {
     //1.Genera un roll random al dado
     const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -46,35 +52,49 @@ btnRoll.addEventListener('click', function(){
     diceEl.src = `img/dice-${dice}.png`;
 
     //3.Checkea si el dado es 1
-    if(dice !== 1){
-        //Si no es 1, agrega el puntaje del dado
-        currentScore += dice;
-        //Va agregando puntaje al activePlayer, siempre y cuando no caiga dado 1
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    if (dice !== 1) {
+      //Si no es 1, agrega el puntaje del dado
+      currentScore += dice;
 
+      //Va agregando puntaje al activePlayer, siempre y cuando no caiga dado 1
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
 
-    } else { //Sino cambia al otro player
-        //cambiando players entre player0 y player1
-        switchPlayer();
+    } else {
+      //Sino cambia al otro player
+      //cambiando players entre player0 y player1
+      switchPlayer();
     }
+  }
 });
 
+
 //Funcionalidad para boton Hold
-btnHold.addEventListener('click', function(){
+btnHold.addEventListener('click', function () {
+  if (playing) {
     //1. Agrega score al jugador activo
     scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
 
     //2. Checkea si el score es >= 100
-    if(scores[activePlayer] >= 100){
-        //termina el juego
-        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    if (scores[activePlayer] >= 100) {
+      //termina el juego
+      playing = false;
+      diceEl.classList.add('hidden');
 
-    } else { //3. Sino cambia de jugador
-        switchPlayer();
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+
+    } else {
+      //3. Sino cambia de jugador
+      switchPlayer();
     }
-
-    
-    
+  }
 });
