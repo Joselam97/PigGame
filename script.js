8939736556076cc9b515ca651b15de13ea6545da
Jose Alvarez <jlam97@hotmail.es>
2024-11-08 20:44:47 -0600

    Logica para cada inicio de tiro de dados, cambio de jugador y suma de scores

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 'use strict';
 
 // Seleccionando elementos
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('.score--1');
+const score1El = document.getElementById('score--1');
 
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -15,10 +18,12 @@ const btnHold = document.querySelector('.btn--hold');
 
 //Iniciando las variables
 score0El.textContent = 0;
-score0El.textContent = 0;
+score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Funcionalidad al dado
 btnRoll.addEventListener('click', function(){
@@ -33,8 +38,17 @@ btnRoll.addEventListener('click', function(){
     if(dice !== 1){
         //Si no es 1, agrega el puntaje del dado
         currentScore += dice;
-        current0El.textContent = currentScore; 
-    } else { //Sino cambia al otro player
+        //Va agregando puntaje al activePlayer, siempre y cuando no caiga dado 1
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
+
+    } else { //Sino cambia al otro player
+        //cambiando players entre player0 y player1
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 });
